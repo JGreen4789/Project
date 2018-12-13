d73d583bc2670c49f0c84f0ac49d16c000be7d7a
JGreen4789 <43245986+JGreen4789@users.noreply.github.com>
2018-12-13 13:54:33 -0500

    Add files via upload

---
+++ MeansAndStandardDeviationsExtracreditQueensBronx.R
@@ -0,0 +1,18 @@
+rt<-a[c(24:42), c(2)]
+mean(rt)
+sd(rt)
+di<-a[c(24:42), c(3)]
+mean(di)
+sd(di)
+fp<-a[c(24:42), c(4)]
+mean(fp)
+sd(fp)
+aa<-a[c(69:80), c(2)]
+mean(aa)
+sd(aa)
+ust<-a[c(69:80), c(3)]
+mean(ust)
+sd(ust)
+jl<-a[c(69:80), c(4)]
+mean(jl)
+sd(jl)
