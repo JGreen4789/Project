11898cd513b14a2374ce8a5db48c42f3fa9a0b7e
JGreen4789 <43245986+JGreen4789@users.noreply.github.com>
2018-12-13 13:47:51 -0500

    Add files via upload

--- CodeForMilestone5.R
+++ CodeForMilestone5.R
@@ -1,2 +1,2 @@
 library(qcc)
-pareto.chart(a$JURISDICTION.NAME, xlab="Zip Codes", main="Pareto chart for frequency of zip codes")
+pareto.chart(table(a$JURISDICTION.NAME), xlab="Zip Codes", main="Pareto chart for frequency of zip codes")
